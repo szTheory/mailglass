59e5972b49b5860924c2fb138a582ed0aca1995f
szTheory <szTheory@users.noreply.github.com>
2026-08-18 21:57:15 -0400

    fix(160-01): start Ecto audit registry

--- scripts/generated_ecto_host_proof.sh
+++ scripts/generated_ecto_host_proof.sh
@@ -247,7 +247,7 @@ migrate_package() {
           "inbound" -> "mailglass_inbound.mailglass_inbound_records"
         end
 
-      {:ok, _started} = Application.ensure_all_started(:postgrex)
+      {:ok, _started} = Application.ensure_all_started(:ecto_sql)
       {:ok, _repo} = Host.Repo.start_link()
       %{rows: [[^relation]]} = Host.Repo.query!("SELECT to_regclass($1)::text", [relation])
 
